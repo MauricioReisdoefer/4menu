f643b238b3396f64436766c7abd1a3994f7c3562
LeoLorde <140649576+LeoLorde@users.noreply.github.com>
2025-08-26 11:08:22 -0300

    style; tela cardapio

--- lib/components/cardapio.dart
+++ lib/components/cardapio.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class Cardapio extends StatefulWidget {
+  final String urlImg;
+
+  const Cardapio({super.key, required this.urlImg});
   @override
   State<Cardapio> createState() => _CardapioState();
 }
 
 class _CardapioState extends State<Cardapio> {
+  Widget _buildLogo(urlImg) {
+    return Image.asset(urlImg);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,9 +26,7 @@ class _CardapioState extends State<Cardapio> {
         scrolledUnderElevation: 0,
         surfaceTintColor: Colors.transparent,
         systemOverlayStyle: SystemUiOverlayStyle.light,
-        title: Center(
-          child: Image.asset('assets/images/4menu.png', height: 150),
-        ),
+        title: Center(child: _buildLogo('images/4chefs.png')),
       ),
     );
   }
